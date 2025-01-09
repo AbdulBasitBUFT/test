87caba9fda2d2227d7fd5f07436a6fcad343a1eb
Abdul Basit <231-007-101@buft.edu.bd>
2025-01-10 00:47:03 +0600

    feat: create main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+element
+= '[' tag-name ',' attributes ',' element-list ']'
+| '[' tag-name ',' attributes ']'
+| '[' tag-name ',' element-list ']'
+| '[' tag-name ']'
+| string
+;
+tag-name
+= string
+;
+attributes
+= '{' attribute-list '}'
+| '{' '}'
+;
+attribute-list
+= attribute ',' attribute-list
+| attribute
+;
+attribute
+= attribute-name ':' attribute-value
+;
+attribute-name
+= string
+;
+attribute-value
+= string
+| number
+| 'true'
+| 'false'
+| 'null'
+;
+element-list
+= element ',' element-list
+| element
+;
